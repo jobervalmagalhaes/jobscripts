75f0071b86d5dc90ba8c65bb707a071e5f3f21e6
Joberval Magalhães dos Santos <29413697+jobervalmagalhaes@users.noreply.github.com>
2025-05-14 13:07:43 -0300

    Deploy website - based on 5e84e1c11c5f8af08579f40e9e25f0e8ba630bdd

---
+++ assets/js/acecf23e.c748e1bf.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjobscripts_site=self.webpackChunkjobscripts_site||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/jobscripts-site/blog","blogTitle":"Blog","authorsListPath":"/jobscripts-site/blog/authors"}')}}]);
